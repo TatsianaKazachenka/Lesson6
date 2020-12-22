c67be7ab8e5161c10af502d760258c393c7986e3
tanuha <asus1986@tut.by>
2020-12-22 15:42:25 +0300

    Fixed AddRemoveElementsTest before refactoring

--- src/test/java/AddRemoveElementsTest.java
+++ src/test/java/AddRemoveElementsTest.java
@@ -10,8 +10,6 @@ public class AddRemoveElementsTest {
     WebDriver driver;
     Utils utils;
     int countBtnRemovableEl = 2;
-    List<WebElement> btnRemovableElements;
-    WebElement btnAddElements;
 
     @BeforeMethod
     public void initTest(){
@@ -27,7 +25,9 @@ public class AddRemoveElementsTest {
 
     @Test
     public void addTest(){
-        addElement();
+        clickOnAddElementButton();
+        //init elements
+        List<WebElement> btnRemovableElements = initRemoveBtn();
         //check result
         String message = "Actual = " + String.valueOf(countBtnRemovableEl) + ", expected = " + String.valueOf(btnRemovableElements.size());
         utils.checkResult(String.valueOf(countBtnRemovableEl), String.valueOf(btnRemovableElements.size()), message);
@@ -35,7 +35,9 @@ public class AddRemoveElementsTest {
 
     @Test
     public void removeTest(){
-        removeElement();
+        clickOnRemoveElementButton();
+        //init elements
+        List<WebElement> btnRemovableElements = initRemoveBtn();
         String message = "Actual = " + String.valueOf(countBtnRemovableEl - 1) + ", expected = " + String.valueOf(btnRemovableElements.size());
         //check result
         utils.checkResult(String.valueOf(countBtnRemovableEl - 1), String.valueOf(btnRemovableElements.size()), message);
@@ -46,30 +48,26 @@ public class AddRemoveElementsTest {
         utils.quitDriver(driver);
     }
 
-    public void addElement(){
-        //init elements
-        initAddBtn();
+    public void clickOnAddElementButton(){
         //click by element
         for(int i = 0; i < countBtnRemovableEl; i++) {
-            utils.elementClick(btnAddElements);
+            utils.elementClick(initAddBtn());
         }
-        //init elements
-        initRemoveBtn();
     }
 
-    public void removeElement(){
-        addElement();
+    public void clickOnRemoveElementButton(){
+        clickOnAddElementButton();
+        //init elements
+        List<WebElement> btnRemovableElements = initRemoveBtn();
         //click by element
         utils.elementClick(btnRemovableElements.get(0));
-        //init elements
-        initRemoveBtn();
     }
 
-    public void initAddBtn(){
-        btnAddElements = utils.getElementByXpath(driver, Constants.AddRemove.ADD_ELEMENTS);
+    public WebElement initAddBtn(){
+        return utils.getElementByXpath(driver, Constants.AddRemove.ADD_ELEMENTS);
     }
 
-    public void initRemoveBtn(){
-        btnRemovableElements = utils.getElementsByClassName(driver, Constants.AddRemove.REMOVEBLE_ELEMENTS);
+    public List<WebElement> initRemoveBtn(){
+        return utils.getElementsByClassName(driver, Constants.AddRemove.REMOVEBLE_ELEMENTS);
     }
 }
